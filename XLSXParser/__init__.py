872d330defa67a78b883ac872eaeb42af54e5bf7
gegemonTV <vovakonki776@gmail.com>
2021-02-11 00:15:12 +0300

    Created Mini Library for Parsing XLSX Files

--- XLSXParser/__init__.py
+++ XLSXParser/__init__.py
@@ -1,3 +1,12 @@
 import pylightxl as xl
 
-xl
+class Parser:
+    def __init__(self, db_name):
+        self.sheet = xl.readxl(fn=db_name).ws_names[0]
+        self.db = xl.readxl(fn=db_name)
+
+    def readName(self):
+        return self.db.ws(self.sheet).address(address="A2") + self.db.ws(self.sheet).address(address="B2") + self.db.ws(self.sheet).address(address="C2")
+
+    def readAnswers(self):
+        return
